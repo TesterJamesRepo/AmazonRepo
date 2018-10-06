da51a4bd2cc098bdba8f3c36e9473f8bb36edb8c
testerjames007 <testerjames007@gmail.com>
2018-10-06 18:07:31 +0530

    first commit

---
+++ amazon/amazonindia/src/main/java/com/qa/amazonindia/amazonindia/App.java
@@ -0,0 +1,13 @@
+package com.qa.amazonindia.amazonindia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
